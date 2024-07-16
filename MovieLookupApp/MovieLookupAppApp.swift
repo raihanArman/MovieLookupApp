6c7da34da82f861455a40b75b0432ce2513ffed5
raihanArman <raihanarman8@gmail.com>
2024-07-16 07:30:33 +0700

    Initial Commit

---
+++ MovieLookupApp/MovieLookupAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieLookupAppApp.swift
+//  MovieLookupApp
+//
+//  Created by Raihan Arman on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieLookupAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
